cd1a865e298c161ac9f3608e79d383f6f818d23f
Emily Marie Purvis <emily.m.purvis@ucdavis.edu>
2023-08-15 12:04:02 -0700

    site 14 plots (first sheet) complete

--- stem_data.R
+++ stem_data.R
@@ -9,6 +9,7 @@ library(tidyverse)
 library(readxl)
 library(sf)
 library(pracma)
+library(dplyr)
 
 #### Set working directory ####
 setwd("C:\\Users\\emily\\Box\\FOCAL\\field data standardization")
@@ -20,46 +21,46 @@ setwd("C:\\Users\\emily\\Box\\FOCAL\\field data standardization")
 # Import sheet 1
 site14 = read_excel(data("stem_data.xls"),sheet=1,col_names = TRUE,skip=2)
 
-# These sites are in UTM zone 10N (crs = 32610) in Willamette NF, the northings are correct but the eastings should be -122...
-# Some of the subplots are in dd.ddddd° format, others are in dd° mm.mmm' format. I converted them all into decimal format manually-- not ideal, I should do it via R function in the future
-
-#create a dataframe of subplot centers-- again, doing this manually is not ideal but I'm unsure of how to extract random cells to make them into a new table
+#create a dataframe of subplot centers-- doing this manually is not ideal but I'm unsure of how to extract random cells to make them into a new table
 subplot_centers <- matrix(c('NW4', -122.447291, 43.597599, 'NE3', -122.446917, 43.597579, 'SW2', -122.444723, 43.597352, 'SE1', -122.446992, 43.597423, 'NW2', -122.447283, 43.59783, 'NE1', -122.44688, 43.597892), ncol=3, byrow=TRUE)
 colnames(subplot_centers) <- c('SUBPLOT','PlotCenterEasting','PlotCenterNorthing')
 subplot_centers <- as.data.frame.matrix(subplot_centers)
 
 #### Map plot centers and save them ####
-subplot_centers_sp <- st_as_sf(subplot_centers, coords = c("PlotCenterEasting", "PlotCenterNorthing"), crs = 32610, remove=F)
-plot(st_geometry(subplot_centers_sp), axes=TRUE)
+subplot_centers_sp <- st_as_sf(subplot_centers, coords = c("PlotCenterEasting", "PlotCenterNorthing"), crs = 4326, remove=F)
 
-# Transforming spatial data points into EPSG 3857 so it's compatible with standard qgis OpenStreetMaps projection
+# Transforming spatial data points into EPSG 3857 so it's compatible with default qgis OpenStreetMaps projection
 st_write (st_transform (subplot_centers_sp, 3857), "C:\\Users\\emily\\Box\\FOCAL\\field data standardization\\stem_data_site14plotcenters.geojson", delete_dsn=TRUE)
 
-# I'm messing something up in this section-- when I bring the geojson into qgis the points are all in some new null island off the coast of s america. Tried various things with no success: also get null island with no crs transformation, tried a lot of coding grammar changes, tried using st_write and st_geometry functions in various ways, tried shapefiles...
+# convert the plot center coordinates to UTM10N format so the calculations in the next step work
+
+subplot_centers_sp_UTM10N <- st_transform (subplot_centers_sp, 32610)
+
+UTM10N_plotcoords <- data.frame(subplot_centers_sp$SUBPLOT, st_coordinates(subplot_centers_sp_UTM10N[,1], st_coordinates(subplot_centers_sp_UTM10N[,2]))) %>% rename (SUBPLOT=subplot_centers_sp.SUBPLOT, EastingUTM10N=X, NorthingUTM10N=Y)
+
+subplot_centers_sp_UTM10N <- full_join(subplot_centers_sp_UTM10N, UTM10N_plotcoords, by="SUBPLOT")
 
 #### Get the coordinates of each tree in Site 14 ####
 
-# add new columns to the Site 14 sheet so that each individual tree has a populated easting and northing
+# add new columns to the Site 14 dataframe so that each individual tree has a populated plot center easting and northing
 
-trees_locations = full_join(site14,subplot_centers,by="SUBPLOT")
+trees_locations = full_join(site14,st_drop_geometry(subplot_centers_sp_UTM10N),by="SUBPLOT")
 
 # calculate the coordinates of each tree using the plot center, distance, and azimuth 
 
 site14_trees = trees_locations %>%
-  mutate(TreeEasting = as.numeric(PlotCenterEasting) + sin(deg2rad(AZIMUTH)) * `DIST (m)`,
-         TreeNorthing = as.numeric(PlotCenterNorthing) + cos(deg2rad(AZIMUTH)) * `DIST (m)`)
+  mutate(TreeEasting = as.numeric(EastingUTM10N) + sin(deg2rad(AZIMUTH)) * `DIST (m)`,
+         TreeNorthing = as.numeric(NorthingUTM10N) + cos(deg2rad(AZIMUTH)) * `DIST (m)`)
 
-# that did not work, the tree eastings and northings are clearly very wrong, trying something else
+#### give trees ID numbers ####
 
-# Calculate theta from azimuth
-trees_locations$theta = trees_locations$AZIMUTH * -1 + 90
+site14_trees = site14_trees %>% mutate(tree_id = 1:nrow(site14_trees))
 
-# Use theta and plot center coordinates to calculate tree coordinates
-trees_locations$TreeEasting = as.numeric(trees_locations$PlotCenterEasting) + trees_locations$`DIST (m)` * cos(trees_locations$theta*pi/180)
+#### convert to spatial data and export ####
 
-trees_locations$TreeNorthing = as.numeric(trees_locations$PlotCenterNorthing) + trees_locations$`DIST (m)` * sin(trees_locations$theta*pi/180)
+trees_sp <- st_as_sf(site14_trees, coords = c("TreeEasting","TreeNorthing"), crs = 32610)
 
-# okay that gave me the same wrong answers....maybe it's something with using decimal degrees instead of degrees/minutes/seconds
+st_write(trees_sp %>% st_transform(3857),data("C:\\Users\\emily\\Box\\FOCAL\\field data standardization\\stem_data_site14trees.geojson"),delete_dsn=TRUE)
 
-
+#### need to figure out how to get rid of duplicate trees ####
 
